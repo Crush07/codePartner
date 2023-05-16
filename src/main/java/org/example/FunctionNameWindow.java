095b70b94d26c7df925983bb239800afa9021032
hyy <2350726260@qq.com>
2023-05-16 22:56:31 +0800

    gridBagLayout布局

--- src/main/java/org/example/FunctionNameWindow.java
+++ src/main/java/org/example/FunctionNameWindow.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import java.awt.*;
 import java.awt.event.InputMethodEvent;
 import java.awt.event.InputMethodListener;
@@ -7,6 +9,7 @@ import javax.swing.event.DocumentListener;
 
 public class FunctionNameWindow extends JFrame {
     private JTextField inputField;
+    private JTextField inputField1;
     private JTextArea displayArea;
 
     public FunctionNameWindow() {
@@ -15,6 +18,7 @@ public class FunctionNameWindow extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         inputField = new JTextField(20);
+        inputField1 = new JTextField(20);
         displayArea = new JTextArea();
         displayArea.setEditable(false);
 
@@ -39,26 +43,40 @@ public class FunctionNameWindow extends JFrame {
             }
         });
 
-        setLayout(new GridBagLayout());
+
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        setLayout(gridBagLayout);
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weightx = 0.5;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(10, 0, 0, 0);
+        gridBagLayout.setConstraints(inputField, gbc);
         add(inputField, gbc);
 
+        gbc.gridx = 1;
+        gbc.insets = new Insets(10, 0, 0, 0);
+        gridBagLayout.setConstraints(inputField1, gbc);
+        add(inputField1, gbc);
+
+        gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.fill = GridBagConstraints.BOTH;
+        gbc.anchor = GridBagConstraints.CENTER;
         gbc.weightx = 1;
         gbc.weighty = 1;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 5;
         gbc.insets = new Insets(10, 10, 10, 10);
+        gridBagLayout.setConstraints(scrollPane, gbc);
         add(scrollPane, gbc);
 
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new FunctionNameWindow());
+        SwingUtilities.invokeLater(FunctionNameWindow::new);
     }
 }
